c86e40676e1c4882277b402eb98f61b731c427f5
gentlemanSoup <anders.horne@gmail.com>
2022-10-19 19:51:27 +0200

    I fix, make thing better

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Pixygon - Debug Tool
+
+0.5.1
+- Added editor-tools for setting up debugprofile if none is present
+- Added automatic, temporary, creation of debugprofiles if none is present
+- Changed name of package
--- Runtime/Log.cs
+++ Runtime/Log.cs
@@ -1,6 +1,10 @@
 using System;
 using UnityEngine;
 using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Pixygon.DebugTool
 {
@@ -10,6 +14,33 @@ namespace Pixygon.DebugTool
 
         private static void LoadDebugProfile() {
             _debugProfiles = Resources.Load<DebugProfiles>("DebugProfiles");
+            if (_debugProfiles == null)
+                CreateDebugProfiles();
+        }
+
+        private static void CreateDebugProfiles() {
+            _debugProfiles = ScriptableObject.CreateInstance<DebugProfiles>();
+            _debugProfiles._addressable = CreateNewDebugProfile("Addressables");
+            _debugProfiles._tablet = CreateNewDebugProfile("Tablet");
+            _debugProfiles._application = CreateNewDebugProfile("Application");
+            _debugProfiles._avatar = CreateNewDebugProfile("Avatar");
+            _debugProfiles._actor = CreateNewDebugProfile("Actor");
+            _debugProfiles._nft = CreateNewDebugProfile("NFT");
+            _debugProfiles._property = CreateNewDebugProfile("Property");
+            _debugProfiles._camera = CreateNewDebugProfile("Camera");
+            _debugProfiles._pixygonApi = CreateNewDebugProfile("PixygonAPI");
+            #if UNITY_EDITOR
+            AssetDatabase.CreateAsset(_debugProfiles, "Assets/Resources");
+            #endif
+        }
+
+        private static DebugProfile CreateNewDebugProfile(string prefix) {
+            var profile = ScriptableObject.CreateInstance<DebugProfile>();
+            profile._prefix = prefix;
+            profile._prefixColor = Random.ColorHSV();
+            profile._hexColor = "#"+ColorUtility.ToHtmlStringRGBA(profile._prefixColor);
+            profile._showLogs = true;
+            return profile;
         }
 
         public static void DebugMessage( DebugGroup group, object message, Object sender = null) {
